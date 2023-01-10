90e08d49e547218b5591e3b0faedbd3aa65d4ced
Uwizeye Jean de Dieu <jeandedieuuwizeye6@gmail.com>
2023-01-10 19:44:51 +0200

    progress on the models Province to Itorero

--- core/web/admin.py
+++ core/web/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User, Province, District, Sector, Cell, Itorero
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Province)
+admin.site.register(District)
+admin.site.register(Sector)
+admin.site.register(Cell)
+    
--- core/web/models.py
+++ core/web/models.py
@@ -2,6 +2,7 @@ import uuid
 from django.db import models
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
 from django.db.models import Q
+from django.urls import reverse
 from django.utils.translation import gettext as _
 
 # Create your models here.
@@ -76,7 +77,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(_("is active"), default=False)
     # a admin user; non super-user
     staff = models.BooleanField(_("staff"), default=False)
-    admin = models.BooleanField(_("admin"), default=False)  # a admin
+    admin = models.BooleanField(_("admin"), default=False) 
     created_on = models.DateTimeField(_("created on"), auto_now_add=True)
     objects = UserManager()
     USERNAME_FIELD = _("username")
@@ -85,7 +86,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     class Meta:
         ordering = ['full_name']
 
-        
+
     def get_full_name(self):
         # The user is identified by their email address
         return self.full_name
@@ -117,3 +118,107 @@ class User(AbstractBaseUser, PermissionsMixin):
         "Is the user a admin member?"
         return self.admin
 
+class Province(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    name = models.CharField(_("name"), max_length=100)
+    created_on = models.DateTimeField(_("created on"), auto_now_add=True)
+    admin = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        verbose_name = _("Province")
+        verbose_name_plural = _("Provinces")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("Province_detail", kwargs={"pk": self.pk})
+
+
+class District(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    name = models.CharField(_("name"), max_length=100)
+    province = models.ForeignKey(Province, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(_("created on"), auto_now_add=True)
+    admin = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        verbose_name = _("District")
+        verbose_name_plural = _("Districts")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("District_detail", kwargs={"pk": self.pk})
+
+
+class Sector(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    name = models.CharField(_("name"), max_length=100)
+    district = models.ForeignKey(District, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(_("created on"), auto_now_add=True)
+    admin = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)    
+
+    class Meta:
+        verbose_name = _("Sector")
+        verbose_name_plural = _("Sectors")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("Sector_detail", kwargs={"pk": self.pk})
+
+
+class Cell(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    name = models.CharField(_("name"), max_length=100)
+    created_on = models.DateTimeField(_("created on"), auto_now_add=True)
+    sector = models.ForeignKey(Sector, on_delete=models.CASCADE)
+    admin = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)    
+
+    class Meta:
+        verbose_name = _("Cell")
+        verbose_name_plural = _("Cells")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("Cell_detail", kwargs={"pk": self.pk})
+
+
+class Itorero(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    name = models.CharField(_("name"), max_length=100)
+    created_on = models.DateTimeField(_("created on"), auto_now_add=True)
+    cell = models.ForeignKey(Cell, on_delete=models.CASCADE)
+    admin = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        verbose_name = _("Itorero")
+        verbose_name_plural = _("Amatorero")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("Itorero_detail", kwargs={"pk": self.pk})
+
+
+class Member(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    itorero = models.ForeignKey(Itorero, verbose_name=_("Itorero arimo"), on_delete=models.CASCADE)
+
+
+    class Meta:
+        verbose_name = _("Member")
+        verbose_name_plural = _("Members")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("Member_detail", kwargs={"pk": self.pk})
