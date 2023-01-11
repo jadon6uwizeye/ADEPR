03b53f6fd1a09298b5fe888904459aebc204d3be
Uwizeye Jean de Dieu <jeandedieuuwizeye6@gmail.com>
2023-01-11 22:32:47 +0200

    changes and issue fixes

--- core/amatorero/admin.py
+++ core/amatorero/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-# admin.site.register(Province)
-# admin.site.register(District)
-# admin.site.register(Sector)
-# admin.site.register(Cell)
-# admin.site.register(Itorero)
+admin.site.register(Province)
+admin.site.register(District)
+admin.site.register(Sector)
+admin.site.register(Cell)
+admin.site.register(Itorero)
--- core/core/settings.py
+++ core/core/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'web',
-    'amatorero'
+    'amatorero',
+    'services'
 ]
 
 MIDDLEWARE = [
