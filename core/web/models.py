9e6c7cc41c362dadfeb5db9d4a0a07a1a11627be
Uwizeye Jean de Dieu <jeandedieuuwizeye6@gmail.com>
2023-01-11 11:02:36 +0200

    recent changes on the models

--- core/web/models.py
+++ core/web/models.py
@@ -207,9 +207,14 @@ class Itorero(models.Model):
         return reverse("Itorero_detail", kwargs={"pk": self.pk})
 
 
+GENDER_CHOICES = (
+    ('M', 'Male'),
+    ('F', 'Female')
+)
 class Member(models.Model):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    gender = models.CharField(_("gender"), max_length=50, choices=GENDER_CHOICES)
     itorero = models.ForeignKey(Itorero, verbose_name=_("Itorero arimo"), on_delete=models.CASCADE)
 
 
@@ -222,3 +227,83 @@ class Member(models.Model):
 
     def get_absolute_url(self):
         return reverse("Member_detail", kwargs={"pk": self.pk})
+
+
+class OfferingCategory(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    name = models.CharField(_("name"), max_length=100)
+    created_on = models.DateTimeField(_("created on"), auto_now_add=True)
+    admin = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+
+    class Meta:
+        verbose_name = _("Offering category")
+        verbose_name_plural = _("Offering categories")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("Offering_category_detail", kwargs={"pk": self.pk})
+
+
+class Offering(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    details = models.TextField()
+    category = models.ForeignKey(OfferingCategory, on_delete=models.SET_NULL, null=True)
+    created_on = models.DateTimeField(_("created on"), auto_now_add=True)
+    recorder = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    itorero = models.ForeignKey(Itorero, on_delete=models.SET_NULL, null=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        verbose_name = _("Offering")
+        verbose_name_plural = _("Offerings")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("Offering_detail", kwargs={"pk": self.pk})
+
+
+class Document(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    name = models.CharField(_("name of the document"), max_length=50)    
+    created_on = models.DateTimeField(_("created on"), auto_now_add=True)
+    admin = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        verbose_name = _("Document")
+        verbose_name_plural = _("Documents")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("Document_detail", kwargs={"pk": self.pk})
+
+STATUS_CHOICES = (
+    ('pending', 'Pending'),
+    ('approved', 'Approved'),
+)
+
+class DocumentRequest(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    created_on = models.DateTimeField(_("created on"), auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    document = models.ForeignKey(Document, on_delete=models.CASCADE)
+    details = models.TextField(_("some details for the document being requested"))
+    status = models.CharField(_("choices"), max_length=50, choices=STATUS_CHOICES)
+    itorero = models.ForeignKey(Itorero, on_delete=models.SET_NULL, null=True)
+
+
+    class Meta:
+        verbose_name = _("Document request")
+        verbose_name_plural = _("Document requests")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("Document_request_detail", kwargs={"pk": self.pk})
